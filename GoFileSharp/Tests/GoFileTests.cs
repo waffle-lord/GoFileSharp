febb028e6f47b706e5bdd257cded30c1665f9a79
waffle.lord <waffle.lord@hotmail.com>
2024-02-26 17:31:57 -0500

    ya mum

--- GoFileSharp/Tests/GoFileTests.cs
+++ GoFileSharp/Tests/GoFileTests.cs
@@ -3,15 +3,33 @@ namespace Tests;
 [TestClass]
 public class GoFileTests
 {
-    
-    [TestMethod]
-    public void GetRootFolder()
+    [AssemblyInitialize]
+    public void Setup(TestContext ctx)
     {
+        //todo: load config data
+    }
+
+    [AssemblyCleanup]
+    public void TearDown(TestContext ctx)
+    {
+        //todo: ya mum
     }
     
+    [TestMethod]
+    public void GetRootFolder(){}
+    
+    [TestMethod]
     public void CreateFolder() {}
+    
+    [TestMethod]
     public void GetFolderById() {}
+    
+    [TestMethod]
     public void FindFile() {}
+    
+    [TestMethod]
     public void FindFolder() {}
+    
+    [TestMethod]
     public void UploadFile() {}
 }
