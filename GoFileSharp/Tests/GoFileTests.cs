b43b08d3054bba7c46cd68e4a1b2d1a517781723
waffle.lord <waffle.lord@hotmail.com>
2024-03-01 14:32:46 -0500

    update tests

--- GoFileSharp/Tests/GoFileTests.cs
+++ GoFileSharp/Tests/GoFileTests.cs
@@ -12,6 +12,14 @@ public class GoFileTests
     private static HttpClient _client = new();
     private static FileInfo _testFile = new FileInfo("../../../Data/test.txt");
 
+    /// <summary>
+    /// A 2sec delay to help reduce any potential rate limiting during testing
+    /// </summary>
+    private static async Task DelayTwoSeconds()
+    {
+        await Task.Delay(2000);
+    }
+
     private static async Task ClearTestFolder()
     {
         Debug.WriteLine("-- Clearing Test Folder --");
@@ -77,13 +85,13 @@ public class GoFileTests
     public static void Teardown()
     {
         Debug.WriteLine("=== Assembly Teardown ===");
-        ClearTestFolder().GetAwaiter().GetResult();
         _client.Dispose();
     }
 
     [TestMethod]
     public async Task GetRootFolder()
     {
+        await DelayTwoSeconds();
         var root = await GoFile.GetMyRootFolderAsync();
 
         Assert.IsNotNull(root);
@@ -96,6 +104,8 @@ public class GoFileTests
     [TestMethod]
     public async Task CreateFolder()
     {
+        await DelayTwoSeconds();
+
         var folderName = "createTest";
         var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
 
@@ -110,6 +120,8 @@ public class GoFileTests
     [TestMethod]
     public async Task GetFolderById()
     {
+        await DelayTwoSeconds();
+
         var folder = await GoFile.GetFolderAsync(_config.TestFolderId);
         
         Assert.IsNotNull(folder);
@@ -121,6 +133,8 @@ public class GoFileTests
     [TestMethod]
     public async Task FindFile()
     {
+        await DelayTwoSeconds();
+
         var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
         var findFileFolder = await testFolder.CreateFolderAsync("findFile");
 
@@ -139,6 +153,8 @@ public class GoFileTests
     [TestMethod]
     public async Task FindFolder()
     {
+        await DelayTwoSeconds();
+
         var name = "searchTest";
         var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
         await testFolder.CreateFolderAsync(name);
@@ -155,6 +171,8 @@ public class GoFileTests
     [TestMethod]
     public async Task UploadFile()
     {
+        await DelayTwoSeconds();
+
         Debug.WriteLine($"Uploading: {_testFile.FullName}");
         
         var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
@@ -168,17 +186,40 @@ public class GoFileTests
     }
 
     [TestMethod]
+    public async Task CopyFileTo()
+    {
+        await DelayTwoSeconds();
+
+        var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
+        var copyFileFolder = await testFolder.CreateFolderAsync("copyFileToSource");
+        var testFile = await copyFileFolder.UploadIntoAsync(_testFile);
+        var copyTargetFolder = await copyFileFolder.CreateFolderAsync("copyFileToTarget");
+
+        var copied = await testFile.CopyToAsync(copyTargetFolder);
+        
+        await copyTargetFolder.RefreshAsync();
+
+        var copiedFile = copyTargetFolder.FindFile(_testFile.Name);
+        
+        Assert.IsTrue(copied);
+        Assert.IsNotNull(copiedFile);
+        Assert.IsTrue(copyTargetFolder.Contents.Count > 0);
+        Assert.IsTrue(_testFile.Length == copiedFile.Size);
+    }
+    
+    [TestMethod]
     public async Task CopyFolderTo()
     {
+        await DelayTwoSeconds();
+
         var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
-        var sourceFolder = await testFolder.CreateFolderAsync("copyToSource");
-        var targetFolder = await testFolder.CreateFolderAsync("copyToTarget");
-
+        var sourceFolder = await testFolder.CreateFolderAsync("copyFolderToSource");
+        var targetFolder = await testFolder.CreateFolderAsync("copyFolderToTarget");
+    
         await sourceFolder.UploadIntoAsync(_testFile);
-
-        // todo: check if /copyContent endpoint recurses folder contents
+        
         var copied = await sourceFolder.CopyToAsync(targetFolder);
-
+    
         await sourceFolder.RefreshAsync();
         await targetFolder.RefreshAsync();
         
@@ -187,19 +228,29 @@ public class GoFileTests
         Assert.IsNotNull(targetFolder.Contents);
         Assert.IsTrue(sourceFolder.Contents.Count > 0);
         Assert.IsTrue(targetFolder.Contents.Count > 0);
-        Assert.IsTrue(sourceFolder.Contents[0].Name == targetFolder.Contents[0].Name);
+    
+        var targetTestFolder = await targetFolder.FindFolderAsync(targetFolder.Contents[0].Name);
+        
+        Assert.IsNotNull(targetTestFolder);
+        Assert.IsTrue(targetTestFolder.Contents.Count > 0);
+        
+        var targetTestFileName = targetTestFolder.Contents[0].Name;
+        
+        Assert.IsTrue(sourceFolder.Contents[0].Name == targetTestFileName);
     }
-
+    
     [TestMethod]
     public async Task CopyFolderInto()
     {
+        await DelayTwoSeconds();
+
         var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
         var sourceFolder = await testFolder.CreateFolderAsync("copyIntoSource");
         var targetFolder = await testFolder.CreateFolderAsync("copyIntoTarget");
-
+    
         await targetFolder.UploadIntoAsync(_testFile);
         await targetFolder.RefreshAsync();
-
+    
         var copied = await sourceFolder.CopyIntoAsync(targetFolder.Contents.ToArray());
         
         Assert.IsTrue(copied);
@@ -207,6 +258,77 @@ public class GoFileTests
         Assert.IsNotNull(targetFolder.Contents);
         Assert.IsTrue(sourceFolder.Contents.Count > 0);
         Assert.IsTrue(targetFolder.Contents.Count > 0);
-        Assert.IsTrue(sourceFolder.Contents[0].Name == targetFolder.Contents[0].Name);
+        
+        var targetTestFolder = await targetFolder.FindFolderAsync(targetFolder.Contents[0].Name);
+
+        Assert.IsNotNull(targetTestFolder);
+        Assert.IsTrue(targetTestFolder.Contents.Count > 0);
+        
+        var targetTestFileName = targetTestFolder.Contents[0].Name;
+        
+        Assert.IsTrue(sourceFolder.Contents[0].Name == targetTestFileName);
+    }
+
+    [TestMethod]
+    public async Task SetFileOptions()
+    {
+        await DelayTwoSeconds();
+
+        var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
+        var fileOptionsFolder = await testFolder.CreateFolderAsync("fileOptions");
+        var testFile = await fileOptionsFolder.UploadIntoAsync(_testFile);
+
+        var directLinkSet = await testFile.SetDirectLink(true);
+
+        await testFile.RefreshAsync();
+        
+        Assert.IsTrue(directLinkSet);
+        Assert.IsNotNull(testFile.DirectLink);
+    }
+
+    [TestMethod]
+    public async Task SetFolderOptions()
+    {
+        var desc = "This is a description";
+        var tags = new List<string>() { "testing", "stuff" };
+        var pass = "test123";
+        var expiry = DateTime.Now.AddDays(5);
+        
+        await DelayTwoSeconds();
+
+        var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
+        var folderOptionsFolder = await testFolder.CreateFolderAsync("folderOptions");
+
+        Assert.IsTrue(await folderOptionsFolder.SetDescription(desc));
+        Assert.IsTrue(await folderOptionsFolder.SetTags(tags));
+        Assert.IsTrue(await folderOptionsFolder.SetPublic(true));
+        Assert.IsTrue(await folderOptionsFolder.SetExpire(expiry));
+        Assert.IsTrue(await folderOptionsFolder.SetPassword(pass));
+    }
+    
+    [TestMethod]
+    public async Task DeleteFolder()
+    {
+        await DelayTwoSeconds();
+
+        var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
+        var delFolder = await testFolder.CreateFolderAsync("delFolder");
+
+        Assert.IsTrue(await delFolder.DeleteAsync());
+        Assert.IsNull(await GoFile.GetFolderAsync(delFolder.Id, true));
+    }
+
+    [TestMethod]
+    public async Task DeleteFile()
+    {
+        await DelayTwoSeconds();
+
+        var testFolder = await GoFile.GetFolderAsync(_config.TestFolderId);
+        var delFileFolder = await testFolder.CreateFolderAsync("delFile");
+        var delFile = await delFileFolder.UploadIntoAsync(_testFile);
+        
+        Assert.IsTrue(await delFile.DeleteAsync());
+        await delFileFolder.RefreshAsync();
+        Assert.IsTrue(delFileFolder.Contents.Count == 0);
     }
 }
